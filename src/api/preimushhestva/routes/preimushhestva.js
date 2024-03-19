28baa7c595ab365431b180ea1d60be4451698ada
Pushcod <4programart1@gmail.com>
2024-03-19 19:43:56 +0300

    Добавлен репозиторий

---
+++ src/api/preimushhestva/routes/preimushhestva.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * preimushhestva router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::preimushhestva.preimushhestva');
